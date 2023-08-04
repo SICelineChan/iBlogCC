30b2f24fee8dfd451bee6ef5926b19123e6541ac
Celine Chan <cctinlone@gmail.com>
2023-08-04 19:27:19 +0200

    starting from beginning

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require("express")
 const cors = require('cors')
-const ejs = require("ejs")
+
+const homeBeginningContent = "This is another blog using the London App Brewery, Dr Angela Yu's Udemy course material. Also with some materials from Hamburg Coding School, Node.js course.";
 
 const app = express();
 
 app.use(cors());
 const port = 3000;
-app.set('view engine', 'ejs')
-
 app.use(express.static("public"));
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: false}))
+
 
 app.get("/", function (req, res) {
-    res.render("index");
+
+    
+    res.render("index", {home: homeBeginningContent});
+    
 });
 
 
